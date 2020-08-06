e72e02801cf0e45d98712cbf8c30e17ebc6083cc
tatadavila <tata.davila.a@gmail.com>
2020-08-06 16:57:33 -0500

    file edited

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -2,41 +2,18 @@ import React from 'react';
 import styled from 'styled-components';
 import { StaticQuery, graphql } from 'gatsby';
 import Img from 'gatsby-image';
-
 import { Section, Container } from '@components/global';
 
 const About = () => (
   <StaticQuery
     query={graphql`
       query {
-        art_fast: file(
-          sourceInstanceName: { eq: "art" }
-          name: { eq: "fast" }
+        banner_build: file(
+          sourceInstanceName: { eq: "banner" }
+          name: { eq: "family" }
         ) {
           childImageSharp {
-            fluid(maxWidth: 760) {
-              ...GatsbyImageSharpFluid_withWebp_tracedSVG
-            }
-          }
-        }
-
-        art_learn: file(
-          sourceInstanceName: { eq: "art" }
-          name: { eq: "learn_yourself" }
-        ) {
-          childImageSharp {
-            fluid(maxWidth: 760) {
-              ...GatsbyImageSharpFluid_withWebp_tracedSVG
-            }
-          }
-        }
-
-        art_ideas: file(
-          sourceInstanceName: { eq: "art" }
-          name: { eq: "ideas" }
-        ) {
-          childImageSharp {
-            fluid(maxWidth: 760) {
+            fluid(maxWidth: 1200) {
               ...GatsbyImageSharpFluid_withWebp_tracedSVG
             }
           }
@@ -44,99 +21,156 @@ const About = () => (
       }
     `}
     render={data => (
-      <Section id="about">
+      <Section id="información">
+        console.log(data);
         <Container>
-          <Grid>
-            <div>
-              <h2>Speed past the competition</h2>
+          <Title>
+            <h2>18 CÓMODOS APARTAMENTOS DE 2 Y 3 ALCOBAS</h2>
+            <h3>
+              55,72 m<sup>2</sup> / 51,88 m<sup>2</sup>
+            </h3>
+            <h3>Ubicado en la Calle 82 entre Carreras 18A y 18B</h3>
+          </Title>
+
+          <Content>
+            <ContentCircle>
               <p>
-                Gatsby.js builds the fastest possible website. Instead of
-                waiting to generate pages when requested, pre-build pages and
-                lift them into a global cloud of servers — ready to be delivered
-                instantly to your users wherever they are.
+                10 apartamentos <br></br>de 55,72 m<sup>2</sup>.
               </p>
-            </div>
-            <Art>
-              <Img fluid={data.art_fast.childImageSharp.fluid} />
-            </Art>
-          </Grid>
-          <Grid inverse>
-            <Art>
-              <Img fluid={data.art_learn.childImageSharp.fluid} />
-            </Art>
-            <div>
-              <h2>Nothing new to learn here</h2>
+              <ul>
+                <li>3 Alcobas</li>
+                <li>2 Baños</li>
+                <li>Sala - Comedor</li>
+                <li>Cocina y labores</li>
+              </ul>
+            </ContentCircle>
+            <ContentCircle>
               <p>
-                Enjoy the power of the latest web technologies – React.js ,
-                Webpack , modern JavaScript and CSS and more — all set up and
-                waiting for you to start building.
+                8 apartamentos <br></br>de 51,88 m<sup>2</sup>
               </p>
-            </div>
-          </Grid>
-          <Grid>
-            <div>
-              <h2>Grow and build your ideas</h2>
-              <p>
-                Waste no more time on tooling and performance. Focus on the the
-                site you want to build and nothing more.
-                <br />
-                <br />
-                Gatsby is fast in every way that matters.
-              </p>
-            </div>
-            <Art>
-              <Img fluid={data.art_ideas.childImageSharp.fluid} />
-            </Art>
-          </Grid>
+              <ul>
+                <li>2 Alcobas</li>
+                <li>1 Estudio</li>
+                <li>1 Baños</li>
+                <li>Sala - Comedor</li>
+                <li>Cocina y labores</li>
+                <li>Mirador</li>
+              </ul>
+            </ContentCircle>
+          </Content>
+
+          {/* <Grid inverse>
+        <Art>
+          <Img fluid={data.art_learn.childImageSharp.fluid} />
+        </Art>
+        <div>
+          <h2>Nothing new to learn here</h2>
+          <p>
+            Enjoy the power of the latest web technologies – React.js , Webpack
+            , modern JavaScript and CSS and more — all set up and waiting for
+            you to start building.
+          </p>
+        </div>
+      </Grid>
+      <Grid>
+        <div>
+          <h2>Grow and build your ideas</h2>
+          <p>
+            Waste no more time on tooling and performance. Focus on the the site
+            you want to build and nothing more.
+            <br />
+            <br />
+            Gatsby is fast in every way that matters.
+          </p>
+        </div>
+        <Art>
+          <Img fluid={data.art_ideas.childImageSharp.fluid} />
+        </Art>
+      </Grid> */}
         </Container>
       </Section>
     )}
   />
 );
 
-const Grid = styled.div`
-  display: grid;
-  grid-template-columns: 3fr 2fr;
-  grid-gap: 40px;
-  text-align: right;
-  align-items: center;
-  justify-items: center;
-  margin: 7px 0;
+const Title = styled.div`
+  text-align: center;
+  ${'' /* color: ${props => props.theme.color.black.regular} */}
+  ${'' /* display: flex;
+  justify-content: center; */};
+`;
 
-  ${props =>
-    props.inverse &&
-    `
-    text-align: left;
-    grid-template-columns: 2fr 3fr;
-  `}
+const Content = styled.div`
+  margin-top: 3rem;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-flex-direction: row;
+  -ms-flex-direction: row;
+  flex-direction: row;
+  -webkit-flex-wrap: nowrap;
+  -ms-flex-wrap: nowrap;
+  flex-wrap: nowrap;
+  -webkit-justify-content: space-around;
+  -ms-flex-pack: distribute;
+  justify-content: space-around;
+  -webkit-align-content: center;
+  -ms-flex-line-pack: center;
+  align-content: center;
+  -webkit-align-items: flex-start;
+  -ms-flex-align: start;
+  align-items: flex-start;
+`;
 
-  h2 {
-    margin-bottom: 16px;
-  }
+const ContentCircle = styled.div`
+  border: 1px solid ${props => props.theme.color.orange.regular};
+  height: 19rem;
+  border-radius: 50%;
+  padding: 3rem;
+  background-color: ${props => props.theme.color.orange.light};
 
-  @media (max-width: ${props => props.theme.screen.md}) {
-    grid-template-columns: 1fr;
-    text-align: left;
-    margin-bottom: 96px;
-
-    &:last-child {
-      margin-bottom: 24px;
-    }
-
-    ${props =>
-      props.inverse &&
-      `
-        ${Art} {
-          order: 2;
-        }
-    `}
+  > p,
+  ul li {
+    color: ${props => props.theme.color.black.regular};
+    font-size: ${props => props.theme.font_size.xsmall};
   }
 `;
 
-const Art = styled.figure`
-  margin: 0;
-  max-width: 380px;
-  width: 100%;
-`;
+// const Grid = styled.div`
+//   display: grid;
+//   grid-template-columns: 3fr 2fr;
+//   grid-gap: 40px;
+//   text-align: right;
+//   align-items: center;
+//   justify-items: center;
+//   margin: 7px 0;
+
+//   ${props =>
+//     props.inverse &&
+//     `
+//     text-align: left;
+//     grid-template-columns: 2fr 3fr;
+//   `}
+
+//   h2 {
+//     margin-bottom: 16px;
+//   }
+
+//   @media (max-width: ${props => props.theme.screen.md}) {
+//     grid-template-columns: 1fr;
+//     text-align: left;
+//     margin-bottom: 96px;
+
+//     &:last-child {
+//       margin-bottom: 24px;
+//     }
+//   }
+// `;
+
+// const Art = styled.figure`
+//   margin: 0;
+//   max-width: 380px;
+//   width: 100%;
+// `;
 
 export default About;
